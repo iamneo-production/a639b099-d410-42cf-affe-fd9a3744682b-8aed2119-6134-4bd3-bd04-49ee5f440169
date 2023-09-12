020e9b8924e9eeea5892eed201f218e1eb01f295
demosuper100 <demosuper58@gmail.com>
2023-09-12 06:37:24 +0000

    qw

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -18,8 +18,7 @@ public class SpringappApplication {
     int diff=obj.sub();
     System.out.println(sum);
     System.out.println(diff);
-    System.out.println("bye");
-		
+		System.out.println("hello");
 	}
 
 }
